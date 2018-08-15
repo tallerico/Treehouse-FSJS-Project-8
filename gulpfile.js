1d8e7af148e1a3afe98599020a47f58605f96c82
Mike Tallerico <mtallerico1@gmail.com>
2018-08-15 08:44:54 -0400

    adds concatScripts task to gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
-const gulp = require("gulp");
+"use strict";
+const gulp = require("gulp"),
+  babel = require("gulp-babel"),
+  concat = require("gulp-concat"),
+  uglify = require("gulp-uglify"),
+  rename = require("gulp-rename"),
+  sass = require("gulp-sass"),
+  maps = require("gulp-sourcemaps"),
+  del = require("del");
+
+gulp.task("concatScripts", () => {
+  return gulp
+    .src(["./js/circle/*.js"])
+    .pipe(maps.init())
+    .pipe(concat("global.js"))
+    .pipe(maps.write())
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("minify", () => {});
+
+gulp.task("clean", function() {
+  del(["dist", "css/application.css*", "js/app*.js*"]);
+});
